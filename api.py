f1359749fe976b309b5a41136a82b17ee0611df5
indiakerle94 <56257161+indiakerle94@users.noreply.github.com>
2021-01-31 11:51:45 +0000

    added index.html

--- api.py
+++ api.py
@@ -1,13 +1,14 @@
-from flask import Flask, request
+from flask import Flask, request, make_response
 from flask_restful import Resource, Api
 import scraper
 
 app = Flask(__name__)
 api = Api(app)
 
-#class Home(Resource):
-#    def get(self):
-#        return make_response(render_template('index.html'))
+class Home(Resource):
+    def get(self):
+        headers = {'Content-Type': 'text/html'}
+        return make_response(render_template('index.html'),200,headers)
 
 class tours(Resource):
     def get(self):
@@ -24,7 +25,7 @@ class guides(Resource):
 
     	return guide
 
-#app.add_resource(home, '/')
+app.add_resource(home, '/')
 api.add_resource(tours, '/tours')
 api.add_resource(guides, '/guides')
 
